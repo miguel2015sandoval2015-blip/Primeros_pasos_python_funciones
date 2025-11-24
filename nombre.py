3d18528a39fd1be6eecf71e02f1c4377622151b4
miguel2015sandoval2015-blip <miguel2015sandoval2015@gmail.com>
2025-11-24 07:39:06 -0500

    Add files via upload

---
+++ nombre.py
@@ -0,0 +1,8 @@
+#***************** zona de funciones *****************************
+#palabra_clave + nombre_funcines(verbo) + parametro(adjectivos)
+def capturar_nombre()
+    nombre_usuario = input("escribir el nombre del cliente: ")
+    return nombre_usuario
+def capturar_rol()
+    rol_usuario = input("escrir¿bir su rol")
+    
