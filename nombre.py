116f480c07a07eec30723c358340089ba2302528
miguel2015sandoval2015-blip <miguel2015sandoval2015@gmail.com>
2025-11-25 09:12:44 -0500

    Delete nombre.py

--- nombre.py
+++
@@ -1,8 +0,0 @@
-#***************** zona de funciones *****************************
-#palabra_clave + nombre_funcines(verbo) + parametro(adjectivos)
-def capturar_nombre()
-    nombre_usuario = input("escribir el nombre del cliente: ")
-    return nombre_usuario
-def capturar_rol()
-    rol_usuario = input("escrir¿bir su rol")
-    
